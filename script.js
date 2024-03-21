bc412f3d12ae979b8d60598722cbacf3b3536464
Anupam Anand <anupamanand2005@gmail.com>
2024-03-21 16:54:00 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+
+const imgs = document.querySelectorAll('.header-slider ul img');
+const prev_btn = document.querySelector('.control_prev');
+const next_btn = document.querySelector('.control_next');
+ let n=0;
+ function changeslide(){
+ for (let i = 0; i < imgs.length; i++) {
+    imgs[i].style.display="none";
+ }
+ imgs[n].style.display= "block";
+}
+changeslide();
+
+prev_btn.addEventListener('click' ,(e)=>{
+if(n > 0){
+    n--;}
+
+    else{
+        n=imgs.length - 1;
+    }
+    changeslide();
+}
+)
+next_btn.addEventListener('click' ,(e)=>{
+    if(n < imgs.length-1){
+        n++;}
+    
+        else{
+            n=0;
+        }
+        changeslide();
+    }
+    )
+
+    
